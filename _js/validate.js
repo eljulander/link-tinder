e2fffc60f3d3ab309ecdf9eae0128b9ad5d814f0
Eric Julander <ericjulander.byui@gmail.com>
2016-11-18 10:13:32 -0700

    Added a timeout which resolves #6

--- _js/validate.js
+++ _js/validate.js
@@ -1,3 +1,17 @@
+function printFlag(){
+    var args = Array.prototype.slice.call(arguments);
+    args.map(function(arg){
+        var message = "";
+        for(var i = 0; i < arg.toString().length+6; i++)
+            message += "*"; 
+        message += "\n*  "+arg+"  *\n";
+        for(var i = 0; i < arg.toString().length+6; i++)
+            message += "*"; 
+        console.log(message);
+    });
+}
+
+
 (function(){
 
 var LinkValidator = function (file) {
@@ -34,39 +48,76 @@ linkValidator.getLinks = function(){
 //links is a cheerio object
 linkValidator.validateLinks = function (links, runNext) {
     var me  = this;
-    function exists(link, index) {
+    var waiting = [];
+    function exists(link, index, next) {
+        waiting.push(index);
         var url = link.attr("href") || link.attr("src");
         var parser = require("url");
         var stringer = require("querystring");
         var options = {
+            method:"head",
             host: parser.parse(url, true).hostname
             ,path: parser.parse(url, true).pathname +"?"+stringer.stringify(parser.parse(url, true).query)
         }
+        var done = false;
         if (!options.host) options.host = "byui.brightspace.com";
         var red = "";
         var callback = function (response) {
             
             if(response.statusCode == 404)
                 me.invalidLinks.push(link);
-            
-            if(index >= links.length-1)
-                runNext();
+            else
+            if(response.statusCode == 403){
+                me.unsure.push(link);
+            }else{
+                if(response.responseUrl != url && ! link.attr("src")){
+                    console.log("Response: ",response.statusCode, ", URL: ", response.responseUrl);
+                    me.unsure.push(link);
+                }
+                
+            }
+            done = true;
+            next();
         }
         
         var http = require("follow-redirects").http;
-        http.request(options, callback).on("error", function(){
-                me.invalidLinks.push(link);
-                console.log("Oops. Err.");
-            
-            if(index >= links.length-1)
-                runNext();
+        var req = http.request(options, callback).on("error", function(){
+            me.invalidLinks.push(link);
+            console.log("Oops. Err.", url);
+            done = true;
+            next();
 
         }).end();
+        
+        setTimeout(function(){
+           // console.log("Triggered", url);
+            if(!done){
+                console.log("Abort Mission!");
+                this.unsure.push(link);
+                next();
+                console.log(me.unsure);
+            }
+            waiting.splice(0,1);
+    //        printFlag(waiting.length);
+            if(waiting.length <= 0)
+                next();
+        },2500);
+        
     }
-    for (var i = 0; i < links.length; i++) {
-        var currentAnchor = links.eq(i);
-        exists(currentAnchor, i);
+    
+    function checkNext(){
+        if(links.length <= 0){
+      //      printFlag("FIN")
+            if(waiting.length > 0) return;
+            printFlag("Finished Processing!")
+            runNext();
+            return;
+        }
+        var currentAnchor = links.eq(0);
+        exists(currentAnchor, 0, checkNext);
+        links.splice(0,1);
     }
+    checkNext();
 }
 
 linkValidator.getBroken = function (links) {
