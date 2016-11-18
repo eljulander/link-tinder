d39b002dc9e08373fee603ffb1a43e0b935562be
Eric Julander <ericjulander.byui@gmail.com>
2016-11-18 16:03:34 -0700

    Added CLI testing.

--- _js/validate.js
+++ _js/validate.js
@@ -1,33 +1,49 @@
-function printFlag(){
-    var args = Array.prototype.slice.call(arguments);
-    args.map(function(arg){
-        var message = "";
-        var length = ((arg.length < 80)? arg.length : 80)+4;
-        for(var i = 0; i < length; i ++) message += "*";
-        var sections = arg.match(/.{1,80}/g);
-        sections.map(function(section){
-            message += "\n*";
-            var minlen = (length - section.length)-2;
-            if(minlen%2 != 0)
-                minlen++;
-            for(var i = 0; i < minlen/2; i ++)message += " ";
-            message += section;
-            for(var i = 0; i < minlen/2; i ++)message += " ";
-            message += "*";
+/*
+  Appends custom flag function to the console
+ */
+console = (function () {
+    //Grabs console for Web or Node
+    var CONSOLE = console.prototype || console;
+
+    /*
+     Prints a star border around the specified text.
+     There is an 80 character limit per line before it wraps.
+     */
+    function printFlag() {
+        var me = this;
+        var args = Array.prototype.slice.call(arguments);
+        args.map(function (arg) {
+            arg = arg.toString();
+            var message = "";
+            var length = ((arg.length < 80) ? arg.length : 80) + 4;
+            for (var i = 0; i < length; i++) message += "*";
+            var sections = arg.match(/.{1,80}/g);
+            sections.map(function (section) {
+                message += "\n*";
+                var minlen = (length - section.length) - 2;
+                for (var i = 0; i < minlen / 2; i++) message += " ";
+                message += section;
+                for (var i = 0; i < (minlen / 2) - minlen % 2; i++) message += " ";
+                message += "*";
+            });
+            message += "\n";
+            for (var i = 0; i < length; i++) message += "*";
+            me.log(message);
         });
-        message += "\n";
-        for(var i = 0; i < length; i ++) message += "*";
-        console.log(message);
-    });
-}
+    }
+    CONSOLE.printFlag = printFlag;
+    return CONSOLE;
+}());
 
 
+var LinkValidator =     
 (function(){
 
 var LinkValidator = function (file) {
     this.file = file;
     this.invalidLinks = [];
     this.unsure = [];
+    this.valid = [];
 }
 var $async = require("async");
 var cheerio = require("cheerio")
@@ -62,6 +78,20 @@ linkValidator.getLinks = function(){
 linkValidator.validateLinks = function (links, runNext) {
     var me  = this;
  
+    function isBroken(url){
+        var urls = ["brainhoney.com",  "box.com"];
+        var broken = false;
+        for(var i in urls){
+            var current = urls[i];
+            if(url.match(new RegExp(current, "g"))){
+                broken = true;
+                break;
+            }
+        }
+        
+        return broken;
+    }
+    
     function exists(link, nextPhase) {
       
         var url = link.attr("href") || link.attr("src");
@@ -86,6 +116,11 @@ linkValidator.validateLinks = function (links, runNext) {
                 if(response.responseUrl != url && ! link.attr("src")){
                     console.log("Response: ",response.statusCode, ", URL: ", response.responseUrl);
                     me.unsure.push(link);
+                }else{
+                    if(isBroken(url))
+                        me.invalidLinks.push(link);
+                    else
+                        me.valid.push(link);
                 }
                 
             }
@@ -119,8 +154,7 @@ linkValidator.validateLinks = function (links, runNext) {
         linkArray.push(links.eq(i));
 
     $async.map(linkArray, exists, function(err, results){
-        if(err) printFlag("There was an err");
-        printFlag("The super awesome thing has been accomplished!");
+        if(err) console.printFlag("There was an err");
         runNext();
     });
     
@@ -128,19 +162,7 @@ linkValidator.validateLinks = function (links, runNext) {
 
 linkValidator.getBroken = function (links) {
     
-    function isBroken(url){
-        var urls = ["brainhoney.com",  "box.com"];
-        var broken = false;
-        for(var i in urls){
-            var current = urls[i];
-            if(url.match(new RegExp(current, "g"))){
-                broken = true;
-                break;
-            }
-        }
-        
-        return broken;
-    }
+    return;
     
     for(var i = 0; i < links.length; i ++){
         var current = links.eq(i);
@@ -164,35 +186,51 @@ linkValidator.removeDuplicates = function(){
         return false;
     });
 }
-
-
-var lv = new LinkValidator("");
-var fs = require("fs");
-var fileReader = fs.createReadStream("./tests.html");
-
-fileReader.on("data", function (chunk) {
-    lv.file += chunk.toString("utf8");
-});
-fileReader.on("end", function () {
-    var anchors = lv.getLinks();
-    var doc = "";
-    for (var i = 0; i < anchors.length; i++) {
-        var currentAnchor = anchors.eq(i).attr("href") || anchors.eq(i).attr("src");
-        doc += currentAnchor + "\n-------------------------------------------------\n";
-    }
-    fs.writeFile("./test.html", doc, function (err) {
-        if (err) throw err;
-    });
-    lv.validateLinks(anchors, function(){
-       
-        console.log("Done Validating Links!\nLinks Found:", lv.invalidLinks.length);
-        lv.removeDuplicates();
-        for(var i = 0; i < lv.invalidLinks.length; i ++){
-            console.log(lv.invalidLinks[i].attr("href") || lv.invalidLinks[i].attr("src"));
-        }
-    });
-    lv.getBroken(anchors);
-
-});
     return LinkValidator;
 }());
+
+if (process.argv[2] === "--test")
+    (function () {
+        var lv = new LinkValidator("");
+        var fs = require("fs");
+        var fileReader = fs.createReadStream(process.argv[3]);
+        fileReader.on("data", function (chunk) {
+            lv.file += chunk.toString("utf8");
+        });
+        fileReader.on("end", function () {
+            var anchors = (process.argv[4] === "-xml") ? lv.getLinksFromXML() : lv.getLinks();
+            // writes all links found to a document
+            var doc = "";
+            for (var i = 0; i < anchors.length; i++) {
+                var currentAnchor = anchors.eq(i).attr("href") || anchors.eq(i).attr("src");
+                doc += currentAnchor + "\n-------------------------------------------------\n";
+            }
+            fs.writeFile("./test.html", doc, function (err) {
+                if (err) throw err;
+            });
+            // prints results of links
+            lv.validateLinks(anchors, function () {
+                console.printFlag("Done Validating Links!\nBroken Links Found: " + lv.invalidLinks.length + "\n" + "Unsure Links: " + lv.unsure.length + "\nCorrect: " + (anchors.length - lv.invalidLinks.length - lv.unsure.length));
+                lv.removeDuplicates();
+                if (lv.invalidLinks.length > 0) {
+                    console.log("\n");
+                    console.printFlag("Broken: ");
+                    for (var i = 0; i < lv.invalidLinks.length; i++) {
+                        console.log(lv.invalidLinks[i].attr("href") || lv.invalidLinks[i].attr("src"));
+                    }
+                }
+                if (lv.unsure.length > 0) {
+                    console.log("\n");
+                    console.printFlag("Unsure:");
+                    for (var i = 0; i < lv.unsure.length; i++) {
+                        console.log(lv.unsure[i].attr("href") || lv.unsure[i].attr("src"));
+                    }
+                }
+                if (lv.valid.length > 0) {
+                    console.log("\n");
+                    console.printFlag("Valid: ");
+                    for (var i in lv.valid) console.log(lv.valid[i].attr("src") || lv.valid[i].attr("src"));
+                }
+            });
+        });
+    }());
